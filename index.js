e522676a798adb2a6d0d89d85c772e1479156e91
MGraat <34764716+MGraat@users.noreply.github.com>
2018-03-29 09:56:21 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+function begroet() {
+    const begroeting = document.getElementById('begroeting');
+    const naam = document.getElementById('naam').value;
+    begroeting.innerText = naam;
+
+    }
